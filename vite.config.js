b0d8ac74b5589b267f550890b25e1147b4cc0492
MahmoudSalih1990 <MAHMOUD.SALIH.WIN11hs@outlook.com>
2024-03-04 05:36:24 +0300

    Initial mit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/repo/",
+  base:"/CRUD-USERS",
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: ['src/main.js', './index.html'],
+      
+  },
+    
+},
 })
